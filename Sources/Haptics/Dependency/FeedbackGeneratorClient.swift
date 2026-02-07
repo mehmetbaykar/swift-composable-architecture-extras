50f02da9932bc061af30cb91b609844513bc5768
Mehmet Baykar <mhbaykar@gmail.com>
2026-02-07 01:11:43 +0100

    Improve Feedbackgenerator

--- Sources/Haptics/Dependency/FeedbackGeneratorClient.swift
+++ Sources/Haptics/Dependency/FeedbackGeneratorClient.swift
@@ -1,76 +1,72 @@
 import Dependencies
+import DependenciesMacros
 import Foundation
 import XCTestDynamicOverlay
 
+@DependencyClient
 public struct FeedbackGeneratorClient: Sendable {
-  public var prepare: @Sendable (HapticFeedback) async -> Void
-  public var generate: @Sendable (HapticFeedback) async -> Void
-
-  public init(
-    prepare: @escaping @Sendable (HapticFeedback) async -> Void,
-    generate: @escaping @Sendable (HapticFeedback) async -> Void
-  ) {
-    self.prepare = prepare
-    self.generate = generate
-  }
+  public var prepare: @MainActor @Sendable (HapticFeedback) async -> Void
+  public var generate: @MainActor @Sendable (HapticFeedback) async -> Void
 }
 
 #if os(iOS)
   import UIKit
 
+  @MainActor private class GeneratorHolder {
+    static let shared = GeneratorHolder()
+    let impactLight = UIImpactFeedbackGenerator(style: .light)
+    let impactMedium = UIImpactFeedbackGenerator(style: .medium)
+    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+    let impactRigid = UIImpactFeedbackGenerator(style: .rigid)
+    let impactSoft = UIImpactFeedbackGenerator(style: .soft)
+    let selectionGenerator = UISelectionFeedbackGenerator()
+    let notificationGenerator = UINotificationFeedbackGenerator()
+  }
+
   extension FeedbackGeneratorClient {
     public static var live: Self {
-      let impactLight = UIImpactFeedbackGenerator(style: .light)
-      let impactMedium = UIImpactFeedbackGenerator(style: .medium)
-      let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
-      let impactRigid = UIImpactFeedbackGenerator(style: .rigid)
-      let impactSoft = UIImpactFeedbackGenerator(style: .soft)
-      let selectionGenerator = UISelectionFeedbackGenerator()
-      let notificationGenerator = UINotificationFeedbackGenerator()
 
-      return Self(
+      return .init(
         prepare: { feedback in
-          await MainActor.run {
-            switch feedback {
-            case .impactLight:
-              impactLight.prepare()
-            case .impactMedium:
-              impactMedium.prepare()
-            case .impactHeavy:
-              impactHeavy.prepare()
-            case .impactRigid:
-              impactRigid.prepare()
-            case .impactSoft:
-              impactSoft.prepare()
-            case .selection:
-              selectionGenerator.prepare()
-            case .success, .warning, .error:
-              notificationGenerator.prepare()
-            }
+          let generatorHolder = GeneratorHolder.shared
+          switch feedback {
+          case .impactLight:
+            generatorHolder.impactLight.prepare()
+          case .impactMedium:
+            generatorHolder.impactMedium.prepare()
+          case .impactHeavy:
+            generatorHolder.impactHeavy.prepare()
+          case .impactRigid:
+            generatorHolder.impactRigid.prepare()
+          case .impactSoft:
+            generatorHolder.impactSoft.prepare()
+          case .selection:
+            generatorHolder.selectionGenerator.prepare()
+          case .success, .warning, .error:
+            generatorHolder.notificationGenerator.prepare()
           }
         },
         generate: { feedback in
-          await MainActor.run {
-            switch feedback {
-            case .impactLight(let intensity):
-              impactLight.impactOccurred(intensity: intensity)
-            case .impactMedium(let intensity):
-              impactMedium.impactOccurred(intensity: intensity)
-            case .impactHeavy(let intensity):
-              impactHeavy.impactOccurred(intensity: intensity)
-            case .impactRigid(let intensity):
-              impactRigid.impactOccurred(intensity: intensity)
-            case .impactSoft(let intensity):
-              impactSoft.impactOccurred(intensity: intensity)
-            case .selection:
-              selectionGenerator.selectionChanged()
-            case .success:
-              notificationGenerator.notificationOccurred(.success)
-            case .warning:
-              notificationGenerator.notificationOccurred(.warning)
-            case .error:
-              notificationGenerator.notificationOccurred(.error)
-            }
+          let generatorHolder = GeneratorHolder.shared
+          switch feedback {
+          case .impactLight(let intensity):
+            generatorHolder.impactLight.impactOccurred(intensity: intensity)
+          case .impactMedium(let intensity):
+            generatorHolder.impactMedium.impactOccurred(intensity: intensity)
+          case .impactHeavy(let intensity):
+            generatorHolder.impactHeavy.impactOccurred(intensity: intensity)
+          case .impactRigid(let intensity):
+            generatorHolder.impactRigid.impactOccurred(intensity: intensity)
+          case .impactSoft(let intensity):
+            generatorHolder.impactSoft.impactOccurred(intensity: intensity)
+          case .selection:
+            generatorHolder.selectionGenerator.selectionChanged()
+          case .success:
+            generatorHolder.notificationGenerator.notificationOccurred(.success)
+          case .warning:
+            generatorHolder.notificationGenerator.notificationOccurred(.warning)
+          case .error:
+            generatorHolder.notificationGenerator.notificationOccurred(.error)
           }
         }
       )
@@ -86,16 +82,14 @@ public struct FeedbackGeneratorClient: Sendable {
       Self(
         prepare: { _ in },
         generate: { feedback in
-          await MainActor.run {
-            let performer = NSHapticFeedbackManager.defaultPerformer
-            switch feedback {
-            case .alignment:
-              performer.perform(.alignment, performanceTime: .default)
-            case .levelChange:
-              performer.perform(.levelChange, performanceTime: .default)
-            case .generic:
-              performer.perform(.generic, performanceTime: .default)
-            }
+          let performer = NSHapticFeedbackManager.defaultPerformer
+          switch feedback {
+          case .alignment:
+            performer.perform(.alignment, performanceTime: .default)
+          case .levelChange:
+            performer.perform(.levelChange, performanceTime: .default)
+          case .generic:
+            performer.perform(.generic, performanceTime: .default)
           }
         }
       )
@@ -111,28 +105,26 @@ public struct FeedbackGeneratorClient: Sendable {
       Self(
         prepare: { _ in },
         generate: { feedback in
-          await MainActor.run {
-            let device = WKInterfaceDevice.current()
-            switch feedback {
-            case .watchNotification:
-              device.play(.notification)
-            case .watchDirectionUp:
-              device.play(.directionUp)
-            case .watchDirectionDown:
-              device.play(.directionDown)
-            case .watchSuccess:
-              device.play(.success)
-            case .watchFailure:
-              device.play(.failure)
-            case .watchRetry:
-              device.play(.retry)
-            case .watchStart:
-              device.play(.start)
-            case .watchStop:
-              device.play(.stop)
-            case .watchClick:
-              device.play(.click)
-            }
+          let device = WKInterfaceDevice.current()
+          switch feedback {
+          case .watchNotification:
+            device.play(.notification)
+          case .watchDirectionUp:
+            device.play(.directionUp)
+          case .watchDirectionDown:
+            device.play(.directionDown)
+          case .watchSuccess:
+            device.play(.success)
+          case .watchFailure:
+            device.play(.failure)
+          case .watchRetry:
+            device.play(.retry)
+          case .watchStart:
+            device.play(.start)
+          case .watchStop:
+            device.play(.stop)
+          case .watchClick:
+            device.play(.click)
           }
         }
       )
